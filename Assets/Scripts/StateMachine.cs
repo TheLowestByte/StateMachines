dfe6456da1010c57ac96b3bed47dade6283cd01f
TheLowestByte <thelowestbyte@gmail.com>
2023-09-17 15:49:28 -0400

    added StateMachine.cs

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public enum State
+    {
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+    State currentState = State.idle;
+    Vector3 lastPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.idle: idle(); break;
+            case State.walking: walking(); break;
+            case State.swimming: swimming(); break;
+            case State.climbing: Climbing(); break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WaterTrigger")
+        {
+            currentState = State.swimming;
+        }
+        else if (other.name == "MountainTrigger")
+        {
+            currentState = State.climbing;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        currentState = State.walking;
+    }
+
+    void idle()
+    {
+        Debug.Log("idle");
+        if(lastPos != transform.position)
+        {
+            currentState = State.walking;
+        }
+    }
+    void walking()
+    {
+        Debug.Log("walking");
+        if (lastPos == transform.position)
+        {
+            currentState = State.idle;
+        }
+        lastPos = transform.position;
+    }
+    void swimming()
+    {
+        Debug.Log("swimming");
+    }
+    void Climbing()
+    {
+        Debug.Log("climbing");
+    }
+}
